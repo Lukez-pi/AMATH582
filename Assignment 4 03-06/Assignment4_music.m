52dcb3ed935202a77bfd318483491cbc40a8925d
Luke Zhu <lukezhu0314@gmail.com>
2020-03-01 14:18:36 -0800

    added function to break music into 5 second clips and stored them in .mat file formtat

--- Assignment 4 03-06/Assignment4_music.m
+++ Assignment 4 03-06/Assignment4_music.m
@@ -1,8 +1,41 @@
 clear, clc, close all
 
 music_genre = ["Classical Music", "Country", "Pop_R&B"];
-for j = 1:length(music_genre)
-    root_dir = strcat("Music/", music_genre(j));
+for i = 1:length(music_genre)
+    convert_wav_to_mat(music_genre(i))
+end
+
+function [] = convert_wav_to_mat(music_genre)
+    file_format = "/*.wav";
+    filter_dir = strcat("Music/", music_genre, file_format);
+    files = dir(filter_dir);
+    artist_music = {files.name};
+    for i = 1:length(artist_music)
+        root_dir = strcat("Music/", music_genre);
+        audio_path = strcat(root_dir, "/", artist_music{i});
+        [y,Fs] = audioread(audio_path);
+        time_interval = 5;
+        feature_num = time_interval * Fs;
+        length_y = length(y);
+        sample_num = floor(length_y / feature_num);
+        truncate_num = mod(length_y, feature_num);
+        data_matrix = reshape(y(1:length_y - truncate_num), feature_num, sample_num);
+        
+        % randomly shuffle the vectors
+        cols = size(data_matrix, 2);
+        P = randperm(cols);
+        data_matrix_rand = data_matrix(P);
+        
+        % save the randomized data matrix
+        artistName_format = split(artist_music{i}, ".");
+        artist_name = artistName_format{1};
+        save_path = strcat(root_dir, "/", artist_name, "_data");
+        save(save_path, "data_matrix_rand");
+    end
+end
+
+function [] = down_sample(music_genre)
+    root_dir = strcat("Music/", music_genre, "*.mp3");
     files = dir(root_dir);
     artist_music = {files.name};
     for i = 3:length(artist_music)
